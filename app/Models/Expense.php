b0dd5e6d81bad7a674ca08dbbb29b04e3598e341
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-03-18 17:59:07 +0100

    Fix `convertMoney` so that it passes tests

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
 
 class Expense extends Model
 {
     use HasFactory;
 
-    public static function convertMoney(int $zlote, int $grosze): int
+    public static function convertMoney(int $zlote, int $grosze = 0): int
     {
+        if ($zlote < 0 || $grosze < 0) {
+            throw new InvalidArgumentException('Arguments must not be negative');
+        }
+
         return $zlote * 100 + $grosze;
     }
 }
