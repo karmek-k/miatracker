5687864a09191be1b325ef84c290453fb1c35aff
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-03-18 18:50:33 +0100

    Create an expense factory

---
+++ database/factories/ExpenseFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Expense>
+ */
+class ExpenseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->text(20),
+            'date' => $this->faker->date(),
+            'money' => $this->faker->numberBetween(10, 10000),
+        ];
+    }
+}
